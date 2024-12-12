09dc5825ce044ad53abe2569e3570d0c5092612a
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-12 04:23:06 +0000

    feat: create Product entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/org/soujava/samples/mongodb/products/Product.java
@@ -0,0 +1,26 @@
+package org.soujava.samples.mongodb.products;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.List;
+
+@Entity
+public class Product {
+
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Manufacturer manufacturer;
+
+    @Column
+    private List<String> tags;
+
+    @Column
+    private Set<Category> categories;
+}
